a051edece3ab56139af61dbdddaf567a6c1ea32e
Keerthi Puthanaveedu <keerthiputhanaveedu@Keerthis-MacBook-Pro.local>
2023-02-25 11:48:36 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Pages from "./pages/Pages";
 import Category from "./components/Categorie";
 import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search";
-import styled from "styled-components";
 
 function App() {
   return (
@@ -16,20 +15,20 @@ function App() {
   );
 }
 
-const Logo = styled.Link`
-  text-decoration: none;
-  font-size: 1.5rem;
-  font-weight: 400;
-  font-family: "Lobster Two", cursive;
-`;
-const Nav = styled.div`
-  padding: 4rem 0rem;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  svg {
-    font-size: 2rem;
-  }
-`;
+// const Logo = styled.Link`
+//   text-decoration: none;
+//   font-size: 1.5rem;
+//   font-weight: 400;
+//   font-family: "Lobster Two", cursive;
+// `;
+// const Nav = styled.div`
+//   padding: 4rem 0rem;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+//   svg {
+//     font-size: 2rem;
+//   }
+// `;
 
 export default App;
